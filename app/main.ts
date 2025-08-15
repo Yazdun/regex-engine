6df6d8d20f050ada33e4be399248162b88f1f738
Yaz <yazduntube@gmail.com>
2025-08-15 12:26:07 +0330

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,117 +1,343 @@
-const args = process.argv;
-const pattern = args[3];
+import { type Token, Tokenizer } from "./tokenizer";
 
-const inputLine: string = await Bun.stdin.text();
+function printTokens(tokens: Token[]): (string | Token)[] {
+  let result: (string | Token)[] = [];
+  for (const token of tokens) {
+    switch (token.type) {
+      case "literal":
+        result.push(token.value);
+        break;
 
-function matchToken(input: string, token: string): boolean {
-  if (!input[0]) return false;
-  switch (true) {
-    case token === ".":
-      return true;
-    case token === "\\d":
-      return /\d/.test(input[0]);
-    case token === "\\w":
-      return /\w/.test(input[0]);
-    case token.startsWith("[") && token.endsWith("]"):
-      const chars = token.slice(1, -1);
-      if (chars.startsWith("^")) {
-        return !chars.slice(1).includes(input[0]);
-      } else {
-        return chars.includes(input[0]);
+      case "quantifier":
+        const innerSimplified = printTokens([token.token]);
+        result.push({ ...token, token: innerSimplified[0] } as Token);
+        break;
+
+      case "alternation":
+        const simplifiedBranches = token.branches.map((branch) =>
+          printTokens(branch),
+        );
+        result.push({ ...token, branches: simplifiedBranches } as Token);
+        break;
+
+      default:
+        result.push(token);
+    }
+  }
+  return result;
+}
+
+class GrepMatcher {
+  private pattern;
+  private tokens: Token[];
+  constructor(pattern: string) {
+    this.pattern = pattern;
+    this.tokens = Tokenizer.tokenize(pattern);
+    console.log(`original pattern: ${pattern}`);
+    console.log(`Tokens: ${JSON.stringify(printTokens(this.tokens))}`);
+  }
+
+  private matchToken(token: Token, input: string[], pos: number): number[] {
+    if (pos > input.length) return [];
+
+    switch (token.type) {
+      case "literal":
+        // if 'a' matches at position 3, next position is [4]
+        // if it doesn't match, no valid positions: []
+        return input[pos] === token.value ? [pos + 1] : [];
+      case "dot":
+        return [pos + 1];
+      case "digit":
+        return /[0-9]/.test(input[pos]) ? [pos + 1] : [];
+      case "word":
+        return /[a-zA-Z0-9_]/.test(input[pos]) ? [pos + 1] : [];
+      case "group":
+        const charMatches = token.chars.includes(input[pos]);
+        // should match means wether the grep should find it.
+        const shouldMatch = token.negated ? !charMatches : charMatches;
+        return shouldMatch ? [pos + 1] : [];
+      case "anchor":
+        if (token.value === "^") return pos === 0 ? [pos] : [];
+        if (token.value === "$") return pos === input.length ? [pos] : [];
+        return [];
+      case "quantifier":
+        return this.matchQuantifier(token, input, pos);
+      case "alternation":
+        return this.matchAlternation(token, input, pos);
+      default:
+        return [];
+    }
+  }
+
+  private matchAlternation(
+    token: Token & { type: "alternation" },
+    input: string[],
+    pos: number,
+  ): number[] {
+    const positions = [];
+    console.log(
+      `Attention: matching alternation for token ${JSON.stringify(printTokens([token]))}`,
+    );
+    for (const branch of token.branches) {
+      let currentPos = pos;
+      let success = true;
+      for (const tokenBranch of branch) {
+        const nextPositions = this.matchToken(tokenBranch, input, currentPos);
+        // if lenght is 0 then it didn't match
+        if (nextPositions.length === 0) {
+          success = false;
+          break;
+        }
+        currentPos = nextPositions[0];
       }
-    default:
-      return input[0] === token;
-  }
-}
-
-function matchPattern(input: string, pattern: string): boolean {
-  if (pattern === "") return true;
-  if (input === "") {
-    if (pattern.endsWith("?")) {
-      return matchPattern(input, pattern.slice(0, -1));
+      if (success) {
+        positions.push(currentPos);
+      }
     }
-    return false;
+    console.log("After matching alternation", positions);
+    return positions;
   }
 
-  let token: string;
-  let restPattern: string;
+  // quantifier allows matching of 0 or None (?) 1+(+) 0+(*)
+  // on our implementation, quantifiers create multiple possible paths.
+  private matchQuantifier(
+    token: Token & { type: "quantifier" },
+    input: string[],
+    pos: number,
+  ): number[] {
+    console.log(
+      `Attention: matching quantifier for token: ${JSON.stringify(printTokens([token]))}`,
+    );
+    const results: number[] = [];
+    if (token.value === "?") {
+      // match 0 or 1 time
+      results.push(pos);
+      const oneMatch = this.matchToken(token.token, input, pos);
+      // if token is literal, this will do a literal match, and advance to pos+1
+      results.push(...oneMatch);
+    } else if (token.value === "*") {
+      // match 0+ times
+      results.push(pos);
+      let currPos = pos;
+      while (true) {
+        const nextPos = this.matchToken(token.token, input, currPos);
+        if (nextPos.length === 0) break;
+        currPos = nextPos[0];
+        results.push(currPos);
+      }
+    } else if (token.value === "+") {
+      // match 1+ times
+      let currPos = pos;
+      const firstMatch = this.matchToken(token.token, input, currPos);
+      if (firstMatch.length === 0) return [];
 
-  switch (true) {
-    case pattern.startsWith("\\d") || pattern.startsWith("\\w"):
-      token = pattern.slice(0, 2);
-      restPattern = pattern.slice(2);
-      break;
+      currPos = firstMatch[0];
+      results.push(currPos);
 
-    case pattern.startsWith("[") && pattern.includes("]"):
-      const end = pattern.indexOf("]") + 1;
-      token = pattern.slice(0, end);
-      restPattern = pattern.slice(end);
-      break;
-
-    default:
-      token = pattern[0];
-      restPattern = pattern.slice(1);
-      break;
-  }
-
-  if (restPattern.startsWith("+")) {
-    restPattern = restPattern.slice(1);
-    let i = 0;
-    while (i < input.length && matchToken(input.slice(i), token)) i++;
-    if (i === 0) return false;
-    for (let j = i; j >= 1; j--) {
-      if (matchPattern(input.slice(j), restPattern)) return true;
+      while (true) {
+        const nextPos = this.matchToken(token.token, input, currPos);
+        if (nextPos.length == 0) break;
+        currPos = nextPos[0];
+        results.push(currPos);
+      }
     }
-    return false;
+
+    return results;
   }
 
-  if (restPattern.startsWith("?")) {
-    restPattern = restPattern.slice(1);
-    if (matchToken(input, token) && matchPattern(input.slice(1), restPattern)) {
+  private matchFromPosition(
+    tokens: Token[],
+    input: string[],
+    tokenIdx: number,
+    inputPos: number,
+  ): boolean {
+    // base case: matched all tokens
+    if (tokenIdx >= tokens.length) {
       return true;
     }
-    return matchPattern(input, restPattern);
-  }
 
-  if (!matchToken(input, token)) return false;
-  return matchPattern(input.slice(1), restPattern);
-}
+    const token = tokens[tokenIdx];
+    console.log(
+      `Matching: \n\t token: ${JSON.stringify(token)} \n\t ${input.slice(inputPos)} \n\t ${tokenIdx} \n\t ${inputPos}`,
+    );
 
-function matchPatternAnywhere(input: string, pattern: string): boolean {
-  if (pattern.startsWith("^")) {
-    const subPattern = pattern.slice(1);
-    if (subPattern.endsWith("$")) {
-      return input === subPattern.slice(0, -1);
+    if (token.type === "quantifier" && token.token.type === "alternation") {
+      return this.matchQuantifiedAlternation(
+        token as Token & { type: "quantifier" },
+        tokens,
+        input,
+        tokenIdx,
+        inputPos,
+      );
     }
-    return matchPattern(input, subPattern);
-  }
 
-  if (pattern.endsWith("$")) {
-    const subPattern = pattern.slice(0, -1);
-    for (let i = 0; i <= input.length; i++) {
-      if (
-        matchPattern(input.slice(i), subPattern) &&
-        i + subPattern.length === input.length
-      ) {
+    const nextPositions = this.matchToken(token, input, inputPos);
+    // try each possible next option
+    for (const pos of nextPositions) {
+      if (this.matchFromPosition(tokens, input, tokenIdx + 1, pos)) {
         return true;
       }
     }
+
     return false;
   }
 
-  for (let i = 0; i <= input.length; i++) {
-    if (matchPattern(input.slice(i), pattern)) return true;
+  private matchQuantifiedAlternation(
+    token: Token & { type: "quantifier" },
+    tokens: Token[], // the full token array
+    input: string[],
+    tokenIdx: number,
+    inputPos: number,
+  ): boolean {
+    const alternation = token.token as Token & { type: "alternation" };
+    const quantifier = token.value;
+    if (quantifier === "?") {
+      // try 0 matches -> skipping the quantified alternation
+      if (this.matchFromPosition(tokens, input, tokenIdx + 1, inputPos)) {
+        return true;
+      }
+      // try 1 match with the alternation
+      for (const branch of alternation.branches) {
+        // create a new token sequence [branch + rest of tokens]
+        const newTokens = [...branch, ...tokens.slice(tokenIdx + 1)];
+        if (this.matchFromPosition(newTokens, input, 0, inputPos)) {
+          return true;
+        }
+      }
+    } else if (quantifier === "*") {
+      // try 0 matches -> skipping the quantified alternation
+      if (this.matchFromPosition(tokens, input, tokenIdx + 1, inputPos)) {
+        return true;
+      }
+      // try 1+ matches (any combination of branches)
+      return this.tryMultipleAlternationMatches(
+        alternation,
+        tokens,
+        input,
+        tokenIdx,
+        inputPos,
+        0, // min matches
+      );
+    } else if (quantifier === "+") {
+      // try 1+ matches (any combination of branches)
+      return this.tryMultipleAlternationMatches(
+        alternation,
+        tokens,
+        input,
+        tokenIdx,
+        inputPos,
+        1, // min matches
+      );
+    }
+    return false;
   }
-  return false;
+
+  private tryMultipleAlternationMatches(
+    alternation: Token & { type: "alternation" },
+    tokens: Token[],
+    input: string[],
+    tokenIdx: number,
+    inputPos: number,
+    minMatches: number,
+    matchCount: number = 0,
+  ) {
+    // if we have met the minimum, continue with the rest of the pattern
+    if (matchCount >= minMatches) {
+      if (this.matchFromPosition(tokens, input, tokenIdx + 1, inputPos)) {
+        return true;
+      }
+    }
+
+    // try matching another instance of the alternation
+    for (const branch of alternation.branches) {
+      let possiblePositions = [inputPos];
+      let success = true;
+
+      // try to match every token for this branch
+      for (const branchToken of branch) {
+        let nextPositions = [];
+
+        // for each poisition we could be at:
+        for (const position of possiblePositions) {
+          const matches = this.matchToken(branchToken, input, position);
+          nextPositions.push(...matches);
+        }
+
+        if (nextPositions.length === 0) {
+          success = false;
+          break;
+        }
+        possiblePositions = nextPositions;
+      }
+      console.log(`success: ${success}`);
+
+      if (success) {
+        for (const endPos of possiblePositions) {
+          if (endPos > inputPos) {
+            // made progress
+            // recursively try to match more instances
+            if (
+              this.tryMultipleAlternationMatches(
+                alternation,
+                tokens,
+                input,
+                tokenIdx,
+                endPos, // continue from where the branch ended
+                minMatches,
+                matchCount + 1,
+              )
+            ) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  match(inputLine: string): boolean {
+    const inputChars = Array.from(inputLine);
+    // check for start anchor
+    if (this.tokens[0]?.type === "anchor" && this.tokens[0]?.value === "^") {
+      // now it must match from the beginning
+      return this.matchFromPosition(this.tokens.slice(1), inputChars, 0, 0);
+    }
+
+    console.log(`input: ${JSON.stringify(inputChars)}`);
+    // otherwise, try matching from each position
+    for (let i = 0; i < inputChars.length; i++) {
+      if (this.matchFromPosition(this.tokens, inputChars, 0, i)) {
+        return true;
+      }
+    }
+
+    // if it hasn't matched on any of the possible paths
+    return false;
+  }
 }
 
-if (args[2] !== "-E") {
-  console.log("Expected first argument to be '-E'");
-  process.exit(1);
+async function main() {
+  const args = process.argv;
+
+  if (args[2] !== "-E") {
+    console.log("Expected first argument to be '-E'");
+    process.exit(1);
+  }
+  const pattern = args[3];
+  const inputLine: string = await Bun.stdin.text();
+
+  try {
+    const matcher = new GrepMatcher(pattern);
+    if (matcher.match(inputLine.trim())) {
+      process.exit(0);
+    } else {
+      process.exit(1);
+    }
+  } catch (error: any) {
+    console.error(`Pattern error: ${error.message}`);
+    process.exit(1);
+  }
 }
 
-if (matchPatternAnywhere(inputLine, pattern)) {
-  process.exit(0);
-} else {
-  process.exit(1);
-}
+main().catch(console.error);
---
+++ app/tokenizer.ts
@@ -0,0 +1,166 @@
+export type Token =
+  | { type: "literal"; value: string }
+  | { type: "dot" }
+  | { type: "word" }
+  | { type: "digit" }
+  | { type: "group"; chars: string[]; negated: boolean }
+  | { type: "anchor"; value: "$" | "^" }
+  | { type: "alternation"; branches: Token[][] }
+  | { type: "quantifier"; value: "?" | "+" | "*"; token: Token };
+
+function splitByPipe(pattern: string[]): string[] {
+  const parts: string[] = [];
+  let depth = 0;
+  let current: string = "";
+  for (let i = 0; i < pattern.length; i++) {
+    if (pattern[i] === "\\" && pattern[i + 1] === "|") {
+      current += "|";
+      i++;
+    } else if (pattern[i] === "|" && depth === 0) {
+      parts.push(current);
+      current = "";
+    } else if (pattern[i] === "(") {
+      current += pattern[i];
+      depth += 1;
+    } else if (pattern[i] === ")") {
+      current += pattern[i];
+      depth -= 1;
+    } else {
+      current += pattern[i];
+    }
+  }
+  parts.push(current);
+  return parts;
+}
+
+export class Tokenizer {
+  static tokenize(pattern: string): Token[] {
+    const chars = Array.from(pattern);
+    const result: Token[] = [];
+    let i = 0;
+
+    while (i < chars.length) {
+      const char = chars[i];
+      switch (char) {
+        case "\\":
+          result.push(Tokenizer.handleSpecialChar(chars[i + 1]));
+          i += 2;
+          break;
+
+        case "(":
+          const [parenToken, parenIdx] = Tokenizer.handleParen(chars, i);
+          result.push(parenToken);
+          i = parenIdx;
+          break;
+
+        case "[":
+          const [bracketToken, bracketIdx] = Tokenizer.handleBracket(chars, i);
+          result.push(bracketToken);
+          i = bracketIdx;
+          break;
+
+        case "^":
+          if (i === 0) {
+            result.push({ type: "anchor", value: "^" });
+            i++;
+          } else {
+            result.push({ type: "literal", value: char });
+            i++;
+          }
+          break;
+
+        case "$":
+          if (i === pattern.length - 1) {
+            result.push({ type: "anchor", value: "$" });
+            i++;
+          } else {
+            result.push({ type: "literal", value: char });
+            i++;
+          }
+          break;
+
+        case ".":
+          result.push({ type: "dot" });
+          i++;
+          break;
+
+        case "?":
+        case "+":
+        case "*":
+          if (result.length === 0) {
+            throw new Error("Quantifier at the start is not allowed");
+          }
+          const prevToken = result.pop()!!;
+          result.push({ type: "quantifier", value: char, token: prevToken });
+          i++;
+          break;
+
+        default:
+          result.push({ type: "literal", value: char });
+          i++;
+          break;
+      }
+    }
+
+    return result;
+  }
+
+  static handleSpecialChar(char: string): Token {
+    if (char === "d") return { type: "digit" };
+    else if (char === "w") return { type: "word" };
+    else return { type: "literal", value: char };
+  }
+
+  static handleParen(chars: string[], i: number): [Token, number] {
+    const closingParenIdx = Tokenizer.findClosingParen(chars, i);
+    if (closingParenIdx === -1) {
+      throw new Error("Unclosed Parentheses");
+    }
+    const groupContent = chars.slice(i + 1, closingParenIdx);
+    const tokenBranches = splitByPipe(groupContent).map((part) =>
+      Tokenizer.tokenize(part),
+    );
+    return [
+      { type: "alternation", branches: tokenBranches },
+      closingParenIdx + 1,
+    ];
+  }
+
+  static handleBracket(chars: string[], i: number): [Token, number] {
+    const closingBracketIdx = chars.indexOf(`]`, i);
+    if (closingBracketIdx === -1) {
+      throw new Error("Unclosed Brackets");
+    }
+    const negated = chars[i + 1] === `^`;
+    const groupChars = negated
+      ? chars.slice(i + 2, closingBracketIdx)
+      : chars.slice(i + 1, closingBracketIdx);
+    return [
+      {
+        type: "group",
+        chars: groupChars,
+        negated: negated,
+      },
+      closingBracketIdx + 1,
+    ];
+  }
+
+  // takes something like word)
+  static findClosingParen(chars: string[], start: number): number {
+    let depth = 1;
+    // +1 to account for the current opening paren
+    let i = start + 1;
+    while (i < chars.length && depth > 0) {
+      const currChar = chars[i];
+      if (currChar === "\\" && i + 1 < currChar.length) {
+        i += 2;
+        continue;
+      }
+      if (currChar === "(") depth += 1;
+      if (currChar === ")") depth -= 1;
+      i++;
+    }
+
+    return depth === 0 ? i - 1 : -1;
+  }
+}
